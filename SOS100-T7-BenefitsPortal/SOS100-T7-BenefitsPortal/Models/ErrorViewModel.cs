9113b24102bfc1a20a1a52bdc2e12de51214938d
Rickard9915 <rist0003@student.hv.se>
2026-03-08 14:38:17 +0100

    Add BenefitsPortal MVC project

---
+++ SOS100-T7-BenefitsPortal/SOS100-T7-BenefitsPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SOS100_T7_BenefitsPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
