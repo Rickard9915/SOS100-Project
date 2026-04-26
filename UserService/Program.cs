ae898b61c4d019931700205e9d3e9502812a2f87
altaj <altajib11@gmail.com>
2026-04-26 12:17:45 +0200

    Added React frontend and fixed CORS + styling

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -8,8 +8,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
-// DbContext (kommer användas snart)
+// DbContext 
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -20,6 +29,7 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+app.UseCors("AllowReactApp");
 
 app.MapControllers();
 using (var scope = app.Services.CreateScope())
